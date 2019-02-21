cb4d394caefbe512b43cf934620cc2bef6fcab11
Pinshuducha <java_zhoulu@163.com>
2019-02-21 15:54:00 +0800

    日志级别

--- src/main/java/com/boot/code/HelloService.java
+++ src/main/java/com/boot/code/HelloService.java
@@ -6,6 +6,6 @@ import org.apache.commons.logging.LogFactory;
 public class HelloService {
     private static Log logger = LogFactory.getLog(HelloService.class);
     public void helloService() {
-        logger.info("service类中的方法");
+        logger.warn("service类中的方法");
     }
 }
