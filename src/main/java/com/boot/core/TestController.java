14a90e9f77084ff71b52e4d8ea458e52f85fbec7
Pinshuducha <java_zhoulu@163.com>
2019-03-06 19:54:54 +0800

    redis单机版连接

--- src/main/java/com/boot/core/TestController.java
+++ src/main/java/com/boot/core/TestController.java
@@ -1,5 +1,9 @@
 package com.boot.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping(value="/hello/test01" , method = RequestMethod.GET)
     public String hello(@RequestParam String name) {
-        return "<<<<<<<<<<name>>>>>>>>>>>>:"+name;
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        operations.set("zhou", "lu");
+        return operations.get("zhou");
     }
 }
