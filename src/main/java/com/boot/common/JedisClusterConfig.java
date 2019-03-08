a74112ba8a813b1e6d5536ce0f1210ef2034bffd
Pinshuducha <java_zhoulu@163.com>
2019-03-08 10:35:34 +0800

    添加redis连接池设置

--- src/main/java/com/boot/common/JedisClusterConfig.java
+++ src/main/java/com/boot/common/JedisClusterConfig.java
@@ -13,8 +13,15 @@ import java.util.Set;
 public class JedisClusterConfig {
     @Bean
     public JedisCluster getJedisCluster() {
+        // 连接池设置
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(200); //最大连接数
+        poolConfig.setMaxIdle(50); //最大空闲连接数
+        poolConfig.setMinIdle(20);  //最小空闲连接数
+        poolConfig.setMaxWaitMillis(1000 * 10); // 最大等待时间 如果超时抛错
+        // 节点集群设置
         Set<HostAndPort> nodes=new HashSet<>();
         nodes.add(new HostAndPort("127.0.0.1", 6379));
-        return new JedisCluster(nodes,10000,20000,2,"123456", new JedisPoolConfig());
+        return new JedisCluster(nodes,10000,20000,2,"123456", poolConfig);
     }
 }
