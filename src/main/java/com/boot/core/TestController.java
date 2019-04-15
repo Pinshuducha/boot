25b66f0d628cb52eaec18719cbd40cb5f22a18f9
zhoulu <java_zhoulu@163.com>
2019-04-15 16:37:52 +0800

    修改提交名称测试

--- src/main/java/com/boot/core/TestController.java
+++ src/main/java/com/boot/core/TestController.java
@@ -1,15 +1,14 @@
 package com.boot.core;
 
 import com.boot.code.Person;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping(value="/hello/test01" , method = RequestMethod.GET)
+    @GetMapping(value="/hello/test01")
     public String hello(@RequestParam String name) {
         Person person = new Person("lisi", "New York");
         return person.toString();
