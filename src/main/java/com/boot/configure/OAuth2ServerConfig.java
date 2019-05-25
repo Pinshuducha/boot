d41dea509d492e33316158c3f8c2ec53f4700289
zhoulu <java_zhoulu@163.com>
2019-05-25 17:40:24 +0800

    oauth2设置token存活时间2

--- src/main/java/com/boot/configure/OAuth2ServerConfig.java
+++ src/main/java/com/boot/configure/OAuth2ServerConfig.java
@@ -56,10 +56,6 @@ public class OAuth2ServerConfig {
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-//        password 方案一：明文存储，用于测试，不能用于生产
-//        String finalSecret = "123456";
-//        password 方案二：用 BCrypt 对密码编码
-//        String finalSecret = new BCryptPasswordEncoder().encode("123456");
             // password 方案三：支持多种编码，通过密码的前缀区分编码方式
             String finalSecret = "{bcrypt}" + new BCryptPasswordEncoder().encode("123456");
             //配置两个客户端,一个用于password认证一个用于client认证
@@ -90,7 +86,10 @@ public class OAuth2ServerConfig {
             tokenService.setSupportRefreshToken(true);
             tokenService.setClientDetailsService(endpoints.getClientDetailsService());
             tokenService.setTokenEnhancer(endpoints.getTokenEnhancer());
+            // 设置token存活时间
             tokenService.setAccessTokenValiditySeconds((int) TimeUnit.SECONDS.toSeconds(60));
+            // 可以使用这个做自动登录时间
+            tokenService.setRefreshTokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(50));
             tokenService.setReuseRefreshToken(false);
             endpoints.tokenServices(tokenService);
         }
