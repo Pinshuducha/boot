f8094f2401aac4a02b61b9336d52b094a5c3f006
zhoulu <java_zhoulu@163.com>
2019-09-27 10:34:55 +0800

    此分支作为boot工程的基础，有其他需求可以在此分支上切出其他分支，目的是避免重复搭建分支

--- src/main/java/com/boot/common/WeiXinProperties.java
+++
@@ -1,8 +0,0 @@
-package com.boot.common;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix="common.weixin")
-public final class WeiXinProperties extends AbstractProperties {
-
-}
--- src/main/java/com/boot/core/TestController.java
+++ src/main/java/com/boot/core/TestController.java
@@ -1,29 +1,14 @@
 package com.boot.core;
 
-import com.boot.utils.TestApi;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @Autowired
-    @Qualifier(value = "weiXinApi")
-    private TestApi weiXinApi;
-    @Autowired
-    @Qualifier(value = "qqApi")
-    private TestApi qqApi;
+
 
     @GetMapping("/hello")
-    public String hello(@RequestParam String flag) {
-        if ("1".equals(flag)) {
-            // 我是微信
-            return weiXinApi.getAppId();
-        } else {
-            // 我是QQ
-            return qqApi.getAppId();
-        }
+    public String hello() {
+       return "test";
     }
 }
